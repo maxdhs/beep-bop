8ae2c20b1ff27e2ad0e182cc88269eae25ba1312
Maxwell Dubin <maxdhs@gmail.com>
2018-10-26 09:44:54 -0700

    Add branches to the beepBop function to code the users input

--- js/scripts.js
+++ js/scripts.js
@@ -1,17 +1,34 @@
+// Backend logic:
+
 var beepBop = function (inputNumber) {
 
-  // Create an array of numbers
+  // Create an array of numbers:
+
   var numberArray = [];
   for (x = 0; x <= inputNumber; x++) {
     numberArray.push(x.toString());
   }
-  return numberArray;
+
+  // Iterate over the array of numbers and transform:
+
+  var codedArray = numberArray.map(function(number) {
+    if (number % 3 == 0 && number != 0) {
+      return "I'm sorry, Dave. I'm afraid I can't do that."
+    }
+    else if (number.includes("1")) {
+      return "Boop!"
+    }
+    else if (number.includes("0")) {
+      return "Beep!"
+    }
+    else {
+      return number;
+    }
+  });
+  return codedArray
 }
 
-
-
-
-
+// Frontend logic:
 
 $(document).ready(function(){
   $("button").click(function(event){
